74132c6e004d18e0c05eff149ae074d3e3e4ddbf
Daniel Petkoski <petkosk1@msu.edu>
2026-03-07 00:43:37 -0500

    semantic analysis baseline

--- src/semantic.rs
+++ src/semantic.rs
@@ -1,1 +1,18 @@
-pub struct SemanticAnalyzer {}
+use crate::ast::{Root, Stmt};
+use crate::compiler::Context;
+
+pub struct SemanticAnalyzer<'ctx> {
+    _ctx: &'ctx Context,
+}
+
+impl<'ctx> SemanticAnalyzer<'ctx> {
+    pub fn analyze(&self, ast: &mut Root) {
+        for stmt in &mut ast.top {
+            self.analyze_statement(stmt);
+        }
+    }
+
+    fn analyze_statement(&self, _stmt: &mut Stmt) {
+        todo!()
+    }
+}

03cd7429bb9e908ecba3636e51b3158a4dc8d78b
Daniel Petkoski <petkosk1@msu.edu>
2026-03-10 18:09:36 -0400

    moving changes to pc

--- src/semantic.rs
+++ src/semantic.rs
@@ -1,20 +1,44 @@
-use crate::ast::{Root, Stmt};
+use crate::ast::{Root, Stmt, StmtKind};
 use crate::compiler::Context;
+use crate::diagnostic::Diagnostic;
 
 pub struct SemanticAnalyzer<'ctx> {
-    _ctx: &'ctx Context,
+    ctx: &'ctx Context,
 }
 
 impl<'ctx> SemanticAnalyzer<'ctx> {
     pub fn new(ctx: &'ctx Context) -> Self {
-        SemanticAnalyzer { _ctx: ctx }
+        SemanticAnalyzer { ctx: ctx }
     }
 
+    // In the future, we could syncronize to catch multiple errors
     pub fn analyze(&mut self, ast: &mut Root) {
         for stmt in &mut ast.top {
-            self.analyze_statement(stmt);
+            match self.analyze_statement(stmt) {
+                Ok(_) => {}
+                Err(diagnostic) => {
+                    self.ctx.diags.borrow_mut().report(diagnostic);
+                    break;
+                }
+            }
         }
     }
 
-    fn analyze_statement(&mut self, _stmt: &mut Stmt) {}
+    fn analyze_statement(&mut self, stmt: &mut Stmt) -> Result<(), Diagnostic> {
+        match &mut stmt.kind {
+            StmtKind::Empty => {}
+            StmtKind::If(cond, do_if, do_else) => self.analyze_if(cond, do_if, do_else)?,
+            _ => todo!(),
+        }
+
+        Ok(())
+    }
+
+    fn analyze_if(
+        &mut self,
+        cond: &mut Box<Expr>,
+        do_if: &mut Box<Stmt>,
+        do_else: &mut Option<Box<Stmt>>,
+    ) -> Result<(), Diagnostic> {
+    }
 }

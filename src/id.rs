7408b6bf90b246fc7b04fd72059d5963a511a43f
Daniel <petkosk1@msu.edu>
2026-03-10 21:46:08 -0400

    loop semantic checks

--- src/id.rs
+++ src/id.rs
@@ -3,3 +3,15 @@ pub struct SymbolID(pub usize);
 
 #[derive(Debug, Clone, Copy)]
 pub struct LoopID(pub usize);
+
+impl LoopID {
+    pub fn dummy() -> Self {
+        LoopID(usize::MAX)
+    }
+
+    pub fn next(&mut self) -> Self {
+        let current = *self;
+        self.0 += 1;
+        current
+    }
+}
--- src/semantic.rs
+++ src/semantic.rs
@@ -1,14 +1,23 @@
 use crate::ast::{Expr, Root, Stmt, StmtKind};
 use crate::compiler::Context;
 use crate::diagnostic::Diagnostic;
+use crate::id::LoopID;
 
 pub struct SemanticAnalyzer<'ctx> {
     ctx: &'ctx Context,
+
+    // Stuff pertaining to loops
+    next_loop_id: LoopID,
+    loop_id_stack: Vec<LoopID>,
 }
 
 impl<'ctx> SemanticAnalyzer<'ctx> {
     pub fn new(ctx: &'ctx Context) -> Self {
-        SemanticAnalyzer { ctx: ctx }
+        SemanticAnalyzer {
+            ctx: ctx,
+            next_loop_id: LoopID(0),
+            loop_id_stack: vec![],
+        }
     }
 
     // In the future, we could syncronize to catch multiple errors
@@ -30,6 +39,7 @@ impl<'ctx> SemanticAnalyzer<'ctx> {
             StmtKind::If(cond, do_if, do_else) => self.analyze_if(cond, do_if, do_else)?,
             StmtKind::ExprStmt(expr) => self.analyze_expr(expr)?,
             StmtKind::Block(stmts) => self.analyze_block(stmts)?,
+            StmtKind::While(id, expr, stmt) => self.analyze_while(id, expr, stmt)?,
             _ => todo!(),
         }
 
@@ -54,6 +64,22 @@ impl<'ctx> SemanticAnalyzer<'ctx> {
         for stmt in stmts {
             self.analyze_statement(stmt)?
         }
+        Ok(())
+    }
+
+    fn analyze_while(
+        &mut self,
+        id: &mut LoopID,
+        expr: &mut Box<Expr>,
+        stmt: &mut Box<Stmt>,
+    ) -> Result<(), Diagnostic> {
+        *id = self.next_loop_id.next();
+        self.loop_id_stack.push(*id);
+
+        self.analyze_expr(expr)?;
+        self.analyze_statement(stmt)?;
+
+        self.loop_id_stack.pop();
         Ok(())
     }
 
